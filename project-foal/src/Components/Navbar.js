ed50dcf62404825445a42ebeeee9b0a05bd9aaf2
Hasan Razib <workrazib@gmail.com>
2026-03-17 21:00:14 +0100

    Nav Components done

---
+++ project-foal/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <div className="logo">Foal</div>
+            <ul className="nav-links">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;

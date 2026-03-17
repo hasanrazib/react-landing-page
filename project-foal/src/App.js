86ddda1ea1cf09178aca322828a4913e32dfa38a
Iftekher Aziz <azizchy02@gmail.com>
2026-03-17 21:49:40 +0100

    Created about us section

--- project-foal/src/App.js
+++ project-foal/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";     
 import './App.css';
 
 function App(){
   return(
     <div className="App">
       <Navbar/>
+      <About/>
     </div>
   );
 

15aa3007ac93a91567c8bf04fee66f20b89db008
Hasan Razib <workrazib@gmail.com>
2026-03-17 21:21:51 +0100

    slider section done

--- project-foal/src/App.js
+++ project-foal/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Slider from "./Components/Slider";
 import './App.css';
 
 function App(){
   return(
     <div className="App">
       <Navbar/>
+      <Slider/>
     </div>
   );
 
---
+++ project-foal/src/Components/Slider.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Slider.css';
+
+const Slider = () => {
+   return(
+    <section className="slider">
+        <div className="slide-content">
+        <h1>Welcome to Our Website</h1>
+        <p>Expert Web Development Solutions</p>
+        <button>Get Started</button>
+      </div>
+    </section>
+   ); 
+
+};
+
+export default Slider;
